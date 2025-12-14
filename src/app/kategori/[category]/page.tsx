1c568bdd1b85116540f89f7c8e248d1ab8d00cc0
Şamil Turgut <samiltrgt07@gmail.com>
2025-12-14 23:12:57 +0300

    ızgara

--- src/app/kategori/[category]/page.tsx
+++ src/app/kategori/[category]/page.tsx
@@ -159,7 +159,7 @@ export default function CategoryPage() {
           {filteredProducts.length > 0 ? (
             <div className={`grid gap-6 ${
               viewMode === 'grid' 
-                ? 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
+                ? 'grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
                 : 'grid-cols-1'
             }`}>
               {filteredProducts.map((product, index) => (
